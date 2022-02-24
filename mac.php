2917f08e7a895507a2a271c3524e4aa3c66b6d03
mohatamimhaque <72484860+mohatamimhaque@users.noreply.github.com>
2022-02-24 23:40:26 +0600

    Create mac.php

---
+++ mac.php
@@ -0,0 +1,25 @@
+<?php
+$mac='UNKNOWN';
+foreach(explode("\n",str_replace(' ','',trim(`getmac`,"\n"))) as $i)
+if(strpos($i,'Tcpip')>-1){$mac=substr($i,0,17);break;}
+echo $mac;
+
+
+
+
+
+echo GetMAC();
+
+function GetMAC(){
+    ob_start();
+    system('getmac');
+    $Content = ob_get_contents();
+    ob_clean();
+    return substr($Content, strpos($Content,'\\')-20, 17);
+}
+
+
+
+
+
+?>
